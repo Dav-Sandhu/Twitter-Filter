611a3e8a948e97d0a26ef3b909485fdc23d6243a
Davanjit Sandhu <davanjit@hotmail.com>
2022-08-12 05:54:50 -0400

    Update Navbar.js

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,12 +1,30 @@
 import Items from './Items'
+import {useState} from 'react'
+import axios from 'axios'
 
 const Navbar = () => {
+
+    const[user, setUser] = useState("")
+
+    const makeRequest = async () => {
+        await axios.post(`http://localhost:8000/addUser`, {user : user.toString()}).then(() => {
+            setTimeout(() => {window.location.reload(false)}, 1250)
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        makeRequest()
+    }
+
     return(
         <div id="navbar" className="navbar">
-            <button id="item-label" className="item-label">Users</button>&nbsp;&nbsp;
+            <button className="item-label">Users</button>&nbsp;&nbsp;
             <Items />
-            <button id="item-label" className="item-label">Add User</button>&nbsp;&nbsp;
-            <button id="item-label" className="item-label">Remove User</button>
+            <form className="add-item" onSubmit={handleSubmit}>
+                <input type="submit" value="Add User"/>
+                <input type="text" value={user} onChange={(e) => setUser(e.target.value)} required />
+            </form>
         </div>
     );
 }
