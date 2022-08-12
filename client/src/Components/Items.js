dd6ffa133c4fb61513ddf6e3dc2304f1d34c6fa3
Davanjit Sandhu <davanjit@hotmail.com>
2022-08-12 05:54:27 -0400

    Update Items.js

--- client/src/Components/Items.js
+++ client/src/Components/Items.js
@@ -1,17 +1,22 @@
-import {useTweet} from '../Context/TweetContext'
+import {useTweet, useSetUser} from '../Context/TweetContext'
 
 const Items = () => {
+  
   const tweets = useTweet()
+  const setUser = useSetUser() 
 
   if (tweets === undefined){
     return <></>
   }
 
   return(
-    <div id="item-list" className="item-list">
+    <div className="item-list">
+      <button className="item" onClick={() => setUser(tweets)}>View All Users</button>
       {tweets.map(i => {
         return(
-          <div id="item" className="item">{i[0].username.toString()}</div>
+          <button className="item" onClick={() => setUser([i])} key={i[0].tweet_id}>
+            {i[0].username}
+          </button>
         )
       })}
     </div>
