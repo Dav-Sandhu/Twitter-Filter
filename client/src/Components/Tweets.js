dad441b88b60633b1146e000c137e4fea6408000
Davanjit Sandhu <davanjit@hotmail.com>
2022-08-12 05:55:18 -0400

    Update Tweets.js

--- client/src/Components/Tweets.js
+++ client/src/Components/Tweets.js
@@ -1,35 +1,45 @@
-import {useTweet} from '../Context/TweetContext'
+import {useUser} from '../Context/TweetContext'
+import axios from 'axios'
 
 const Tweets = () => {
-    const tweets = useTweet()
+  const tweets = useUser()
+  let unique_key = -3;
 
-    if (tweets === undefined){
-        return <></>
-    }
+  if (tweets === undefined){
+    return <></>
+  }
 
-    return(
-        tweets.map(user => 
-            user.map(t => 
-                {return(
-                    <div className="border">
-                        <div className="tweet">
-                            <h3 className="profile">
-                                <img className="profile_picture" src={t.profile_picture} alt="" />&nbsp;
-                                <div className="username">{t.username}</div>
-                            </h3>
-                            <div className="content">{t.text}</div><br />
-                            {
-                                t.img_flag === "true" ? t.images.map(i => {
-                                    return <img className="images" src={i} alt=""/>
-                                }) : <></>
-                            }
-                            <div className="date_posted">{t.date_posted}</div><br />
-                        </div>
-                    </div> 
-                )}    
-            )
-        )
+  const removeUser = async (u) => {
+    await axios.post(`http://localhost:8000/removeUser`, {user : u.toString()}).then(() => {
+      setTimeout(() => {window.location.reload(false)}, 1250)
+  })
+  }
+
+  return(
+    tweets.map(user => 
+      user.map(t => 
+        {return(
+          <div className="border" key={t.tweet_id}>
+            <div className="tweet">
+              <h3 className="profile">
+                <img className="profile_picture" src={t.profile_picture} alt="" />&nbsp;
+                <div className="username">{t.username}</div>
+              </h3>
+              <div className="content">{t.text}</div><br />
+              {
+                t.img_flag === "true" ? t.images.map(i => {
+                  return <img className="images" src={i} key={unique_key-- - t.tweet_id} alt=""/>
+                }) : <></>
+                  
+              }
+              <div className="date_posted">{t.date_posted}</div>
+              <button className="remove_button" onClick={() => {removeUser(t.screen_name)}}>Remove User</button><br />
+            </div>
+          </div> 
+        )}    
+      )
     )
+  )
 }
 
 export default Tweets
