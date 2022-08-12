8090de3bfd96df2e347f00fac265932702fd79b3
Davanjit Sandhu <davanjit@hotmail.com>
2022-08-12 05:57:04 -0400

    Update TweetContext.js

--- client/src/Context/TweetContext.js
+++ client/src/Context/TweetContext.js
@@ -2,23 +2,41 @@ import axios from 'axios'
 import React, {useState, useEffect, useContext} from 'react'
 
 const TweetContext = React.createContext()
+const UserContext = React.createContext()
+const SetUserContext = React.createContext()
 
 export const useTweet = () => {
   return useContext(TweetContext)
 }
 
+export const useUser = () => {
+  return useContext(UserContext)
+}
+
+export const useSetUser = () => {
+  return useContext(SetUserContext)
+}
+
 const TweetProvider = ({children}) => {
   const [tweets, setTweets] = useState()
+  const [user, setUser] = useState()
 
   useEffect(() => {
     axios.get(`http://localhost:8000`).then(res => {
-      setTweets(JSON.parse(JSON.stringify(res))['data']['tweets'])
+      const output = JSON.parse(JSON.stringify(res))['data']['tweets']
+
+      setTweets(output)
+      setUser(output)
     });
   }, [])
 
   return (
-    <TweetContext.Provider value={tweets} >
-      {children}
+    <TweetContext.Provider value={tweets}>
+      <UserContext.Provider value={user}>
+        <SetUserContext.Provider value={setUser}>
+          {children}
+        </SetUserContext.Provider>
+      </UserContext.Provider>
     </TweetContext.Provider>
   )
 
